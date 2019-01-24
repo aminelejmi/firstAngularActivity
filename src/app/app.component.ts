5a6623482b77945de186b08c0f6929e628854dc9
aminelejmi <malejmi@dom.tti>
2019-01-24 15:16:43 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'activite1';
+    allPosts = [
+        {
+            title: 'Mon titre 1',
+            content: 'Mon contenu 1',
+            loveIts: 0,
+            created_at: new Date()
+        },
+        {
+            title: 'Mon titre 2',
+            content: 'Mon contenu 2',
+            loveIts: 0,
+            created_at: new Date()
+        },
+        {
+            title: 'Mon titre 3',
+            content: 'Mon contenu 3',
+            loveIts: 0,
+            created_at: new Date()
+        },
+    ];
 }
